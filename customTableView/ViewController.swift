4ba85888864b7aa23123dd63e0a71fad68264d3d
takuya <takuya93210@yahoo.co.jp>
2018-04-05 20:00:08 +0900

    Initial Commit

---
+++ customTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  customTableView
+//
+//  Created by 大室拓也 on 2018/04/05.
+//  Copyright © 2018年 taku9321. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
